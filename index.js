13237bb7a8d7fcad738f9008346ec1b8bc58fb1a
SimbCo <simbco@Kevins-MacBook-Pro.local>
2019-10-23 16:53:55 -0700

    initial commit - basic set up for constant - reducer flow

---
+++ index.js
@@ -0,0 +1,32 @@
+import C from './constants' 
+import { allSkiDays } from './store/reducers'
+
+const state = [
+    {
+        "resort": "Kirkwood",
+        "date": "2016-13-15",
+        "powder": true,
+        "backcountry": false
+    },
+    {
+        "resort": "Boreal",
+        "date": "2016-12-15",
+        "powder": false,
+        "backcountry": true
+    }
+]
+
+const action = {
+    type: C.REMOVE_DAY,
+    payload: "2016-12-15"
+}
+
+const nextState = allSkiDays(state, action)
+
+console.log(`
+
+    initial state: ${JSON.stringify(state)}
+    action: ${JSON.stringify(action)}
+    new state: ${JSON.stringify(nextState)}
+
+`)
